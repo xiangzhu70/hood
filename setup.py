2fc4050b4f55cca5bf5797c26aab5ac0d044fe9d
Xiang Zhu <xzhu70@gmail.com>
2022-05-14 19:58:36 -0700

    fix python package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hood',
+    version='0.0.0',
+    author='Xiang Zhu',
+    author_email='xzhu70@gmail.com',
+    packages=['hood'],
+    #scripts=['bin/script1','bin/script2'],
+    url='https://github.com/xiangzhu70/hood',
+    license='AGPL-3.0',
+    description='A Hierarchical Object-Oriented Diagnostics Framework (HOOD)',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
