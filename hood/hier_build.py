4344701e00f4843b6ca8bcc5225e762fbced2fbc
Xiang Zhu <xzhu70@gmail.com>
2023-05-13 13:42:00 -0700

    hier_build add checks and cmds into node_diag.py

--- hood/hier_build.py
+++ hood/hier_build.py
@@ -168,6 +168,18 @@ class HierNode:
                     props_line += " "*12 + f"\"{prop.name}{prop.range_str}\", \n"
                 props_line += " "*8 + "]"
                 f.write(f"{props_line}\n")
+                # write checks
+                checks_line = " "*8 + "self.checks = [\n"
+                for check in self.checks_list:
+                    checks_line += " "*12 + f"\"{check.name}\", \n"
+                checks_line += " "*8 + "]"
+                f.write(f"{checks_line}\n")
+                # write commands
+                commands_line = " "*8 + "self.commands = [\n"
+                for command in self.commands_list:
+                    commands_line += " "*12 + f"\"{command.name}\", \n"
+                commands_line += " "*8 + "]"
+                f.write(f"{commands_line}\n")
 
     def create_property_file(self):
         if not len(self.props_list):
