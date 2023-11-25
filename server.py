1fce437592d040664fa7938baed74508ea805781
Xiang Zhu <xzhu70@gmail.com>
2023-11-25 08:28:48 -0800

    serve globally, not just localhost

--- server.py
+++ server.py
@@ -128,6 +128,7 @@ if __name__ == "__main__":
 
     socketio.run(app,
         debug=False,
+        host='0.0.0.0',
         port=5001,
         # ssl_context=('/home/xiang/cert/server.crt',
         #   '/home/xiang/cert/server.key')
